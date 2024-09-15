4f1c4167be06124a319d9e87614b5a533fd8cbbc
Subhayan Chatterjee <subho0137@gmail.com>
2024-09-15 20:59:34 +0530

    Add files via upload

---
+++ C-Programming pattern examples/C program to create Hollow Right Angle Triangle star pattern.cpp
@@ -0,0 +1,30 @@
+// C program to create Hollow Right Angle Triangle star pattern
+
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    for(i=1; i<=rows; i++)
+    {
+        for(j=1; j<=i; j++)
+        { 
+            if(j==1 || j==i || i==rows)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
