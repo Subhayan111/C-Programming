26cd428c2d3930e34eaea016e9e97798fef81c8c
Subhayan Chatterjee <subho0137@gmail.com>
2024-09-18 22:15:59 +0530

    Add files via upload

---
+++ C-Programming pattern examples/C program to create Mirrored Hollow Right Angle Triangle star pattern.cpp
@@ -0,0 +1,33 @@
+// C program to create Mirrored Hollow Right Angle Triangle star pattern
+
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+    printf("Enter number of rows : ");
+    scanf("%d", &rows);
+
+    for(i=1; i<=rows; i++)
+    {
+        for(j=i; j<rows; j++)
+        {
+            printf(" ");
+        }
+
+        for(j=1; j<=i; j++)
+        {
+            if(i==rows || j==1 || j==i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
