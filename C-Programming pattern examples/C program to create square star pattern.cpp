e8bfd33de1a457570313f364c571097f3bab1425
Subhayan Chatterjee <subho0137@gmail.com>
2024-09-21 11:45:23 +0530

    Add files via upload

---
+++ C-Programming pattern examples/C program to create square star pattern.cpp
@@ -0,0 +1,27 @@
+// C program to create square star pattern
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+
+    printf("Enter number of rows:");
+    scanf("%d", &N);
+
+    
+    for(i=1; i<=N; i++)
+    {
+        
+        for(j=1; j<=N; j++)
+        {
+            
+            printf("*");
+        }
+        
+        
+        printf("\n");
+    }
+
+    return 0;
+}
