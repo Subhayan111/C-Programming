8e178809b0bde972c0f667f33a88d4df5f711737
Subhayan Chatterjee <subho0137@gmail.com>
2025-04-24 13:00:43 +0530

    Add files via upload

---
+++ C-Programming common examples/Check whether a year is leap year or not.cpp
@@ -0,0 +1,23 @@
+// Check whether a year is leap year or not
+#include<stdio.h>
+int main()
+{
+	int yr;
+	printf("Enter a year:");
+	scanf("%d",&yr);
+	if(yr%100==0)
+	{
+		if(yr%400==0)
+			printf("It is a Leap year");
+		else
+			printf("It is not a Leap year");
+	} 
+	else
+	{
+		if(yr%4==0)
+			printf("It is a Leap year");
+		else
+			printf("It is not a Leap year");
+	}
+return 0;
+}
