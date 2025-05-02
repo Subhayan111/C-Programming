f653b1ca6a73aa62f6a5d7f087fda19ad8b7749e
Subhayan Chatterjee <subho0137@gmail.com>
2025-05-02 20:51:59 +0530

    Add files via upload

---
+++ C-Programming common examples/Check whether a year is leap or not using conditional operators.cpp
@@ -0,0 +1,13 @@
+// Check whether a year is leap or not using conditional operators
+#include<stdio.h>
+int main()
+{
+	int year;
+	printf("Enter a year:");
+	scanf("%d",&year);
+	if(year%400==0 || year%100!=0 && year%4==0)
+		printf("The year is leap year\n");
+	else
+		printf("The year is not leap year\n");
+	return 0;
+}
