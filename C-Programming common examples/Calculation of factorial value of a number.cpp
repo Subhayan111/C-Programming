597fe12a1601ada850eb6f6d1c4b7c354275ee1a
Subhayan Chatterjee <subho0137@gmail.com>
2025-06-02 12:07:16 +0530

    Add files via upload

---
+++ C-Programming common examples/Calculation of factorial value of a number.cpp
@@ -0,0 +1,16 @@
+// Calculation of factorial value of a number
+#include<stdio.h>
+int main()
+{
+	int num,i,fact;
+	printf("Enter a integer number:");
+	scanf("%d",&num);
+	fact=i=1;
+	while(i<=num)
+	{
+		fact=fact*i;
+		i++;
+	}
+	printf("Factorial value of %d is = %d\n",num,fact);
+	return 0;
+}
